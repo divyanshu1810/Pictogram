47e3895094cbb71cae9384b482f0c28ac93583b6
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-10-04 15:45:42 +0530

    :sparkles: Added category schema
    Added category schema

--- travel-blog-backend/schemas/schema.js
+++ travel-blog-backend/schemas/schema.js
@@ -4,6 +4,7 @@ import post from './post'
 import blockContent from './blockContent'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import category from './category'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -14,6 +15,7 @@ export default createSchema({
   types: schemaTypes.concat([
     /* Your types here! */
     post,
-    blockContent
+    blockContent,
+    category
   ]),
 })

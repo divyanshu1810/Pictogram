e990dad95b02d7b9fc16bb8778d814dd9547bac3
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-10-04 13:55:31 +0530

    :art: Added post Schema
    Post Schema Added

---
+++ travel-blog-backend/schemas/blockContent.js
@@ -0,0 +1,14 @@
+export default {
+    name:'blockContent',
+    type:'array',
+    title:'Block Content',
+    of:[
+        {
+            type:'block',
+            title:'Block',
+            style:[
+                {title:'Normal',value:'normal'}
+            ]
+        }
+    ]
+}
---
+++ travel-blog-backend/schemas/post.js
@@ -0,0 +1,60 @@
+export default {
+    name:'post',
+    type:'document',
+    title:'Blog Post',
+    fields:[
+        {
+            name:'postedAt',
+            type:'geopoint',
+            title:'Location',
+        },
+        {
+            name:'title',
+            type:'string',
+            title:'Title',
+        },
+        {
+            name:'slug',
+            type:'slug',
+            title:'Slug',
+            options:{
+                source:'title',
+                maxLength:96
+            },
+        },
+        // {
+        //     name:'author',
+        //     type:'reference',
+        //     title:'Author',
+        //     to:{
+        //         type:'author'
+        //     }
+        // },
+        {
+            name:'mainImage',
+            type:'image',
+            title:'Main Image',
+            option:{
+                hotspot:true,
+            },
+        },
+        // {
+        //     name:'categories',
+        //     type:'array',
+        //     title:'Categories',
+        //     // of:[
+        //     //     {type:'reference' , to:{type:'category'}}
+        //     // ]
+        // },
+        {
+            name:'publishedAt',
+            type:'datetime',
+            title:'Published At',
+        },
+        // {
+        //     name:'body',
+        //     type:'blockContent',
+        //     title:'Body',
+        // }
+    ]
+}

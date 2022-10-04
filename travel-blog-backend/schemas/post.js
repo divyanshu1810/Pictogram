d68b7e13d17d596d3537f81b123ab1a8451f823f
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-10-05 00:28:24 +0530

    :sparkles: Leaflet & Backend Completed
    Added Maps using leaflet js and completed the travel-blog-backend along with the data.

--- travel-blog-backend/schemas/post.js
+++ travel-blog-backend/schemas/post.js
@@ -1,7 +1,28 @@
+const getPosition = (options) =>{
+    if(navigator.geolocation){
+        return new Promise ((resolve,reject) => {
+            navigator.geolocation.getCurrentPosition(resolve,reject,options)
+        })
+    }
+}
+
 export default {
     name:'post',
     type:'document',
     title:'Blog Post',
+    initialValue:async() =>({
+        postedAt:await getPosition()
+        .then(({coords}) => {
+            const {latitude , longitude , altitude} = coords;
+            return {
+                _type:'geopoint',
+                lat:latitude,
+                lng:longitude,
+                alt:altitude || undefinded
+            }
+        })
+        .catch(()=>undefined)
+    }),
     fields:[
         {
             name:'postedAt',

c5c6080379c69c045487ba61f71b34636176afcd
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-10-05 10:52:58 +0530

    :alien: Added next/sanity and other packages
    added sanity packages and other dependencies along with setting up sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,32 @@
-const Home = () => {
+import {getClient} from '../lib/sanity.server'
+import groq from 'groq'
+const Home = ({posts}:any) => {
   return (
     <div>
       
     </div>
   )
+}
+
+export async function getStaticProps({preview = false}:any){
+  const posts = await getClient(preview).fetch(groq`
+    *[_type == 'post' && publishedAt < now()] | order(publishedAt desc){
+      _id,
+      title,
+      "username":author->username,
+      "categories":category[]->{id,title},
+      "authorImage":author->avatar,
+      body,
+      mainImage,
+      slug,
+      publishedAt
+    }
+  `)
+  return {
+    props:{
+      posts,
+    },
+  }
 }
 
 export default Home

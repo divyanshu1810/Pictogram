b4b2be7fe6ba1fab64cae4c7fddc0174d2530f3d
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-10-05 18:04:42 +0530

    :sparkles: Created Static Paths for Posts
    Created Static Paths for posts and fetched authors and posts details using groq

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,7 +1,46 @@
-const post = () => {
+import groq from "groq"
+import Tag from "../../components/Tag"
+import { PortableText } from "@portabletext/react"
+import { urlFor } from "../../lib/sanity"
+import { getClient } from "../../lib/sanity.server"
+const post = ({post}:any) => {
+    console.log(post)
     return (
         <div></div>
     )
 }
 
+const query = groq`
+    *[_type == "post" && slug.current == $slug][0]{
+        title,
+        "username":author->username,
+        "about":author->bio,
+        "categories":categories[]->{id,title},
+        "authorImage":author->avatar,
+        body,
+        publishedAt,
+        mainImage,
+        postedAt
+    }`
+
+export async function getStaticPaths(){
+    const paths = await getClient().fetch(
+        groq`*[_type == "post" && defined(slug.current)][].slug.current`
+    )
+
+    return {
+        paths:paths.map((slug : any)=>({params:{slug}})),
+        fallback:true,
+    }
+}
+
+export async function getStaticProps({params,preview = false}:any){
+    const post =  await getClient(preview).fetch(query,{slug:params.slug,})
+
+    return {
+        props : {
+            post,
+        }
+    }
+}
 export default post

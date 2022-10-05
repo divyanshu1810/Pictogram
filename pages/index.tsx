2c80d68f40df995e8d4c64d7eb24b78b528e0ff8
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-10-05 20:46:32 +0530

    :rocket: Completed
    Completed the Blog Micro-site using Sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,25 @@
-import {getClient} from '../lib/sanity.server'
+import { getClient } from '../lib/sanity.server'
 import groq from 'groq'
 import Head from 'next/head'
-import Link from 'next/link'
 import Card from '../components/Card'
-const Home = ({posts}:any) => {
-  // console.table(posts)
+const Home = ({ posts }: any) => {
   return (
     <div className='dashboard w-[100%]'>
       <Head>
         <title>Travel Blog</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <h1>{posts.username}</h1>
       <div className='posts-container flex flex-wrap justify-center'>
-        {posts?.map((post:any)=>(
-          <Link href="" key={post._id}>
-            <Card post={post}/>
-          </Link>
+        {posts?.map((post: any) => (
+            <Card post={post} key={post._id}/>
         ))}
       </div>
-
     </div>
   )
 }
 
-export async function getStaticProps({preview = false}:any){
+export async function getStaticProps({ preview = false }: any) {
   const posts = await getClient(preview).fetch(groq`
     *[_type == 'post' && publishedAt < now()] | order(publishedAt desc){
       _id,
@@ -39,7 +34,7 @@ export async function getStaticProps({preview = false}:any){
     }
   `)
   return {
-    props:{
+    props: {
       posts,
     },
   }
